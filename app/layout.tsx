186fdfba1b16d11622d113d952b5d213dcbaae16
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-03-29 00:09:53 +0900

    Add: react-toastifyを使用するための設定を実施

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layouts/Header";
+import ToastProvider from "@/lib/react-toastify/ToastProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <ToastProvider>
+          <Header />
+          {children}
+        </ToastProvider>
       </body>
     </html>
   );
